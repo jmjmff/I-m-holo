ad804dc969815c1467318bb5ddd116196313a57b
hanseunggyu <161230776+hanseunggyu@users.noreply.github.com>
2025-05-26 14:57:52 +0900

    Update auth.py
    암호화 X
    비밀번호 평문

--- auth.py
+++ auth.py
@@ -1,45 +1,12 @@
-# import bcrypt
-# from db import get_connection
-
-# def register_user(email, password, nickname):
-#     conn = get_connection()
-#     cursor = conn.cursor()
-#     hashed_pw = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
-#     try:
-#         cursor.execute("INSERT INTO users (email, password, nickname) VALUES (%s, %s, %s)",
-#                        (email, hashed_pw, nickname))
-#         conn.commit()
-#         return True
-#     except:
-#         return False
-#     finally:
-#         cursor.close()
-#         conn.close()
-
-# def login_user(email, password):
-#     conn = get_connection()
-#     cursor = conn.cursor()
-#     cursor.execute("SELECT password FROM users WHERE email = %s", (email,))
-#     result = cursor.fetchone()
-#     cursor.close()
-#     conn.close()
-
-#     if result:
-#         return bcrypt.checkpw(password.encode(), result[0].encode())
-#     return False
-
-
-import bcrypt
 from db import get_connection
 
 def register_user(email, password, nickname):
     conn = get_connection()
     cursor = conn.cursor()
-    hashed_pw = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
     try:
         cursor.execute(
             "INSERT INTO users (email, password, nickname) VALUES (%s, %s, %s)",
-            (email, hashed_pw, nickname)
+            (email, password, nickname)
         )
         conn.commit()
         return True
@@ -59,5 +26,5 @@ def login_user(email, password):
     conn.close()
 
     if result:
-        return bcrypt.checkpw(password.encode(), result[0])  # 이미 bytes 타입임
+        return password == result[0]
     return False
