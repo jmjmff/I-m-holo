a2169264a80b9e29fa79674ec8f97f8dcf1b4544
minseon-01 <mseon5284@gmail.com>
2025-05-26 16:58:21 +0900

    sql 연결 성공

--- db.py
+++ db.py
@@ -1,31 +1,10 @@
-# import mysql.connector
-
-# def get_connection():
-#     return mysql.connector.connect(
-#         host='127.0.0.1',
-#         port=3306,
-#         user='proj-1',
-#         passwd='admin',
-#         charset='utf8',
-#         database= 'proj-1',
-#         autocommit=True
-    
-#     )
-
-# if __name__ == "__main__":
-#     try:
-#         conn = get_connection()
-#         print("✅ MySQL 연결 성공!")
-#         conn.close()
-#     except Exception as e:
-#         print("❌ 연결 실패:", e)
 import mysql.connector
 
 def get_connection():
     conn = mysql.connector.connect(
         host='127.0.0.1',
         port=3306,
-        user='proj-1',
+        user='project',
         passwd='admin',
         charset='utf8',
         database='proj-1'
